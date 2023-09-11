497ee60b3a35353b1d6579a4decfce3a15929dc7
Davidbjjj <pondavid103@gmail.com>
2023-09-11 14:41:10 -0300

    feat:add desafio ContaBanco

---
+++ src/ContaTerminal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class ContaTerminal {
+    public static void main(String[] args) {
+
+        Scanner sc= new Scanner(System.in);
+
+        int numero=0;
+        String agencia;
+        String nomeCliente;
+
+
+        System.out.println("Digite seu numero");
+        numero= sc.nextInt();
+
+        System.out.println("Digite sua agencia");
+        agencia= sc.next();
+
+        System.out.println("Digite seu Nome");
+        nomeCliente= sc.next();
+
+        System.out.println("Digite seu saldo");
+        double saldo = sc.nextDouble();
+
+
+        System.out.println("Olá "+ nomeCliente +", obrigado por criar uma conta em nosso banco, sua agência é "+ agencia +", conta "+ numero +" e seu saldo "+ saldo +" já está disponível para saque");
+
+
+    }
+}

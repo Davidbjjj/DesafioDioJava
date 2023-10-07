436ad2499815dd479dc2db228e707c40dc14312e
David Pontes <pondavid103@gmail.com>
2023-10-07 10:05:41 -0300

    refactory: add outro metodo get e set

--- src/PraticaDePOO/SitemCadastro.java
+++ src/PraticaDePOO/SitemCadastro.java
@@ -3,6 +3,9 @@ package PraticaDePOO;
 public class SitemCadastro {
     public static void main(String[] args) {
         Pessoa pessoa = new Pessoa("1180", "David");
-        System.out.println(pessoa.getCpf()+"-"+ pessoa.getNome());
+        /** Sempre que for colocar algo "set"*/
+        pessoa.setNumero("(81)988770146");
+        /** Sempre que for pegar algo "set"*/
+        System.out.println(pessoa.getCpf()+"-"+ pessoa.getNome()+"-"+pessoa.getNumero());
     }
 }
